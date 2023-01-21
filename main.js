d4309860d3196dd3de264527038a43a56d5e78d4
mosmn <ithinkisee8@gmail.com>
2023-01-22 05:15:13 +0800

    setup prettier

--- main.js
+++ main.js
@@ -5,8 +5,14 @@ function Book(title, author, pages, readStatus) {
   this.readStatus = readStatus;
 }
 
-Book.prototype.info = () => `${this.title} by ${this.author}, ${this.pages} pages, ${this.readStatus}`;
+Book.prototype.info = () =>
+  `${this.title} by ${this.author}, ${this.pages} pages, ${this.readStatus}`;
 
-const RichDadPoorDad = new Book('Rich Dad Poor Dad', 'Robert Kiyosaki', 300, 'not read yet');
+const RichDadPoorDad = new Book(
+  "Rich Dad Poor Dad",
+  "Robert Kiyosaki",
+  300,
+  "not read yet"
+);
 
 console.log(RichDadPoorDad.info());
