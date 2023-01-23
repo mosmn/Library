ca35faddd56c1d967c3c47fb711bac078bd2d292
mosmn <ithinkisee8@gmail.com>
2023-01-23 15:23:15 +0800

    organized the code and added the showForm and hideForm features

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const layouts = document.querySelectorAll(`.layout > div`);
 const bookContainer = document.querySelector(`.book-container`);
+let labels = null;
 
 const createLabels = () => {
   const labels = document.createElement(`div`);
@@ -14,21 +15,50 @@ const createLabels = () => {
   return labels;
 };
 
-let labels = null;
+const handleLayoutClick = (e) => {
+  const layoutClass = e.target.className;
+  bookContainer.classList.remove("grid", "list");
+  bookContainer.classList.add(layoutClass);
+
+  if (layoutClass === "list") {
+    if (!labels) {
+      labels = createLabels();
+    }
+  } else if (labels) {
+    labels.remove();
+    labels = null;
+  }
+}
 
 layouts.forEach((layout) => {
-  layout.addEventListener("click", (e) => {
-    const layoutClass = e.target.className;
-    bookContainer.classList.remove("grid", "list");
-    bookContainer.classList.add(layoutClass);
-
-    if (layoutClass === "list") {
-      if (!labels) {
-        labels = createLabels();
-      }
-    } else if (labels) {
-      labels.remove();
-      labels = null;
-    }
-  });
+  layout.addEventListener("click", handleLayoutClick);
 });
+
+const addBook = document.querySelector(`.add-book`);
+const form = document.querySelector(`form`);
+const closeBtn = document.querySelector(`.close`);
+
+const showForm = () => {
+  form.style.display = "flex";
+};
+
+const hideForm = () => {
+  form.style.display = "none";
+  form.reset();
+};
+
+addBook.addEventListener("click", showForm);
+closeBtn.addEventListener("click", hideForm);
+
+const handleWindowClick = (e) => {
+  if (
+    e.target !== form &&
+    e.target !== closeBtn &&
+    e.target !== addBook &&
+    !form.contains(e.target)
+  ) {
+    hideForm();
+  }
+}
+
+window.addEventListener("click", handleWindowClick);
