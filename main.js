ee95291a8aeef61e46877b5cadc500d20703ed6a
mosmn <ithinkisee8@gmail.com>
2023-01-23 11:11:27 +0800

    styled both layouts

--- main.js
+++ main.js
@@ -1,22 +1,34 @@
-const layouts = document.querySelectorAll(`.layout div`);
+const layouts = document.querySelectorAll(`.layout > div`);
 const bookContainer = document.querySelector(`.book-container`);
 
+const createLabels = () => {
+  const labels = document.createElement(`div`);
+  labels.classList.add(`labels`);
+  labels.innerHTML = `
+    <div class="title">Title</div>
+    <div class="author">Author</div>
+    <div class="pages">Pages</div>
+    <div class="status">Status</div>
+  `;
+  bookContainer.insertBefore(labels, bookContainer.firstChild);
+  return labels;
+};
+
+let labels = null;
+
 layouts.forEach((layout) => {
-  layout.addEventListener(`click`, (e) => {
-    if (e.target.classList.contains(`grid`)) {
-      if (bookContainer.classList.contains(`list`)) {
-        bookContainer.classList.remove(`list`);
-        bookContainer.classList.add(`grid`);
-      } else {
-        bookContainer.classList.add(`grid`);
+  layout.addEventListener("click", (e) => {
+    const layoutClass = e.target.className;
+    bookContainer.classList.remove("grid", "list");
+    bookContainer.classList.add(layoutClass);
+
+    if (layoutClass === "list") {
+      if (!labels) {
+        labels = createLabels();
       }
-    } else if (e.target.classList.contains(`list`)) {
-      if (bookContainer.classList.contains(`grid`)) {
-        bookContainer.classList.remove(`grid`);
-        bookContainer.classList.add(`list`);
-      } else {
-        bookContainer.classList.add(`list`);
-      }
+    } else if (labels) {
+      labels.remove();
+      labels = null;
     }
   });
 });
